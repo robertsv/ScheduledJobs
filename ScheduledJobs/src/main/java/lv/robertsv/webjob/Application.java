f72e8cb0d1fbfae24e13aa99f1a40168012f201a
roberts_v <Roberts.Vartins@Knowledgeprice.com>
2016-03-30 23:00:21 +0300

    Start using Speedment and dozer

--- ScheduledJobs/src/main/java/lv/robertsv/webjob/Application.java
+++ ScheduledJobs/src/main/java/lv/robertsv/webjob/Application.java
@@ -1,7 +1,12 @@
 package lv.robertsv.webjob;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.dozer.DozerBeanMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +14,28 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
-	
+
+	// @Bean
+	// public DozerBeanMapperFactoryBean
+	// dozerBeanMapperFactoryBean(@Value("classpath*:mappings/*mappings.xml")
+	// Resource[] resources) throws Exception {
+	// final DozerBeanMapperFactoryBean dozerBeanMapperFactoryBean = new
+	// DozerBeanMapperFactoryBean();
+	// // Other configurations
+	//// dozerBeanMapperFactoryBean.setMappingFiles(resources);
+	// return dozerBeanMapperFactoryBean;
+	// }
+
+	@Bean(name = "org.dozer.Mapper")
+	public DozerBeanMapper dozerBean() {
+		List<String> mappingFiles = Arrays.asList("dozer-global-configuration.xml", "dozer-bean-mappings.xml",
+				"more-dozer-bean-mappings.xml");
+
+		DozerBeanMapper dozerBean = new DozerBeanMapper();
+		dozerBean.setMappingFiles(mappingFiles);
+		return dozerBean;
+	}
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
--- ScheduledJobs/src/main/java/lv/robertsv/webjob/dao/JobDao.java
+++ ScheduledJobs/src/main/java/lv/robertsv/webjob/dao/JobDao.java
@@ -1,29 +1,56 @@
 package lv.robertsv.webjob.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
+import org.dozer.DozerBeanMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.speedment.Manager;
+import com.speedment.Speedment;
+
+import lv.robertsv.speedment.test.TestApplication;
+import lv.robertsv.speedment.test.webjobs.public_.scheduled_job.ScheduledJob;
 import lv.robertsv.webjob.domain.Job;
 
 @Repository
 public class JobDao {
-
-	// TODO (RV): add impl
-	private static List<Job> JOBS = new ArrayList();
+	
+    private Manager<ScheduledJob> manager;
+    
+    @Autowired
+    private DozerBeanMapper mapper;
+    
+    @PostConstruct
+    public void init() {
+    	Speedment speedment = new TestApplication().withPassword("postgres").build();
+        manager = speedment.managerOf(ScheduledJob.class);
+	}
 
 	public List<Job> getAll() {
-		return JOBS;
+		List<ScheduledJob> scheduledJobs =  manager.stream().collect(Collectors.toList());
+		
+		System.out.println(mapper);
+		// TODO (RV): use dozer to convert
+		return null;
 	}
 
 	public void add(Job job) {
-		JOBS.add(job);
+		ScheduledJob scheduledJob = manager.newInstance()
+	    .setEnabled(1)
+	    .setId(777L)
+	    .setPath(job.getPath())
+	    .setSchedule(job.getSchedule())
+	    .setStatus("Scheduled")
+	    .persist();
+		
 	}
 
 	public void remove(long jobId) {
-		JOBS = JOBS.stream().filter(j -> j.getId() != jobId).collect(Collectors.toList());
+		manager.stream().filter(j -> j.getId().equals(jobId)).forEach(j -> j.remove());
 	}
 
 }
